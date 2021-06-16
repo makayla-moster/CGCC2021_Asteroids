38a9055fb29ea57d211312d487845bc7503eadb1
Makayla Moster <monkmonster96@gmail.com>
2021-06-16 19:14:46 -0400

    u

--- SpaceRocks_Unfinished_NoExplosion/MainScene/Main.gd
+++ SpaceRocks_Unfinished_NoExplosion/MainScene/Main.gd
@@ -26,3 +26,13 @@ func spawn_rock(size, pos=null, vel=null):
 	r.screensize = screensize 
 	r.start(pos, vel, size)
 	$Rocks.add_child(r)
+	r.connect('exploded', self, 'on_Rock_exploded')
+
+func on_Rock_exploded(size, radius, pos, vel):
+	if size <= 1:
+		return
+	for offset in [-1, 1]:
+		var dir = (pos - $Player.position).normalized().tangent() * offset 
+		var newpos = pos + dir * radius
+		var newvel = dir * vel.length() * 1.1 
+		spawn_rock(size - 1, newpos, newvel)
